bda9bc2521d627c2105475e77b621ccd1d569a74
Nazar Slavych <slavuch232355@gmail.com>
2025-02-12 19:04:27 +0200

    add model Worker

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Position(models.Model):
@@ -5,3 +6,10 @@ class Position(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username
