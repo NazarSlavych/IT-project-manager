5be040964f0e703a1adcb6fdb66fd751be573dbd
Nazar Slavych <slavuch232355@gmail.com>
2025-02-12 20:13:19 +0200

    fix Worker model, add null and blank

--- manager/models.py
+++ manager/models.py
@@ -9,7 +9,7 @@ class Position(models.Model):
 
 
 class Worker(AbstractUser):
-    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.username
