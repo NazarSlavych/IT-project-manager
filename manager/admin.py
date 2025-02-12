6e73309169a858247aeae784fe9ee9e57f3a8c4b
Nazar Slavych <slavuch232355@gmail.com>
2025-02-12 22:00:28 +0200

    Update admin model Task

--- manager/admin.py
+++ manager/admin.py
@@ -16,6 +16,11 @@ class WorkerAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("position",)}),)
 
 
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "priority", "task_type",)
+    list_filter = ("task_type",)
+
+
 admin.site.register(Position)
 admin.site.register(TaskType)
-admin.site.register(Task)
