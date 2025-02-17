0b8c9f6872afcc04c3826014e309bfa80c8a65c2
Nazar Slavych <slavuch232355@gmail.com>
2025-02-17 18:43:32 +0200

    Create def index with context

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,16 @@
+from django.db.models import Count
 from django.shortcuts import render
 
-# Create your views here.
+from manager.models import Task, Worker
+
+
+def index(request):
+    num_task = Task.objects.count()
+    num_worker = Worker.objects.count()
+    num_free_workers = Worker.objects.annotate(task_count=Count("workers")).filter(task_count=0).count()
+    context = {
+        "num_task": num_task,
+        "num_worker": num_worker,
+        "num_free_workers": num_free_workers,
+    }
+    return render(request, "manager/index.html", context)
