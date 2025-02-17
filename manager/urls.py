74a82dec25e4e4cb330fd30f35ef57344c002d4b
Nazar Slavych <slavuch232355@gmail.com>
2025-02-17 19:40:28 +0200

    Add path workers

--- manager/urls.py
+++ manager/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import index, TaskListView
+from .views import index, TaskListView, WorkersListView
+
+
 
 urlpatterns = [
     path("", index, name="index"),
     path("tasks", TaskListView.as_view(), name="tasks"),
+    path("workers", WorkersListView.as_view(), name="workers"),
 ]
 
 
