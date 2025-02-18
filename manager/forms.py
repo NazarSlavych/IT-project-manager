6053c6fed6c1931d696cece54b6855ad859518b7
Nazar Slavych <slavuch232355@gmail.com>
2025-02-18 20:21:41 +0200

    Add search field in workers and tasks list

--- manager/forms.py
+++ manager/forms.py
@@ -28,3 +28,21 @@ class TaskForm(forms.ModelForm):
         widgets = {
             "assignees": forms.CheckboxSelectMultiple(),
         }
+
+
+class TaskSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name.."})
+    )
+
+
+class WorkerSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username.."})
+    )
