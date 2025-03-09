002f9b668bbaca109be33b6fb52512beb0b9df13
Nazar Slavych <slavuch232355@gmail.com>
2025-03-09 15:16:20 +0200

    Put secret data such as SECRET_KEY into environment variables (.env file)

--- configs/settings.py
+++ configs/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i^6(i2#)_#lji&-b^k@l5&rs=l8%8lo+vxin+(3br*z6y4t(=s"
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG", "False") == "True"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # SECURITY WARNING: don`t run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
